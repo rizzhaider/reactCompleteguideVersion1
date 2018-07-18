0fddc4065cfc1116ddd1b40ff1ade01a83f1771a
rizzhaider <rizwan02riz@gmail.com>
2018-07-18 06:58:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Radium, {StyleRoot} from 'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -13,29 +14,32 @@ class App extends Component {
 
     showPersons:false,
   }
-  switchNameHandler = (newNmae) =>{
-    console.log(this.state)
-    //DONT Do This--  this.state.persons[0].name = "ali"
-    this.setState({
-      persons:[
-        {name:newNmae, age:26},
-        {name:"messi", age: 31},
-        {name:"haider", age: 26}
-      ]
-    })
-  }
+  // switchNameHandler = (newNmae) =>{
+  //   console.log(this.state)
+  //   //DONT Do This--  this.state.persons[0].name = "ali"
+  //   this.setState({
+  //     persons:[
+  //       {name:newNmae, age:26},
+  //       {name:"messi", age: 31},
+  //       {name:"haider", age: 26}
+  //     ]
+  //   })
+  // }
 
 
-  chnageNameHandler = (event) =>{
-    console.log(this.state)
-    //DONT Do This--  this.state.persons[0].name = "ali"
-    this.setState({
-      persons:[
-        {name:"rizz", age:26},
-        {name:event.target.value, age: 31},
-        {name:"haider", age: 26}
-      ]
-    })
+  chnageNameHandler = (event, id) =>{
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id == id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+    this.setState({persons:persons})
   }
 
   toggleHandler = () => {
@@ -44,18 +48,23 @@ class App extends Component {
 
   }
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
+    // const persons = this.state.persons.slice(); --this is way to copy object...
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons:persons})
   }
   render() {
     const style = {
-      backgroundColor:'white',
+      backgroundColor:'green',
+      color:'white',
       font:'inherit',
       border:'1px solid blue',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover': {
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     }
     let persons = null;
 
@@ -63,27 +72,42 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person name={person.name}
+            return <Person  click = {() => this.deletePersonHandler(index)}
+                            name={person.name}
                             age={person.age}
                             key={person.id}
-                            click={() => this.deletePersonHandler(index)} />
+                            changed={(event) => this.chnageNameHandler(event, person.id)} />
           })}
            
         </div> 
-      )
-     
+      );
+     style.backgroundColor = 'red';
+     style[':hover'] = {
+       backgroundColor : 'salmon',
+       color :'black'
+     }
       
     }
+    const classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red'); // classes =  [red];
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold') // classes = [red, bold]
+    }
+
     return (
+      <StyleRoot>
       <div className="App">
        <h1>Hello moto</h1>
-       <p> This is really working bro!</p>
+       <p className={classes.join(' ')}> This is really working bro!</p>
        <button style={style} onClick={this.toggleHandler}>Toggle Button</button>
-      {persons}      
+        {persons}      
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'hello messi'))
   }
 }
 
-export default App;
+export default Radium(App);
